209bfdf3fab225663feaddd94a13455fdd286819
Soulyyy <soulyyy@gmail.com>
2016-05-10 22:01:56 +0300

    Added test case where maximal != maximum

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,28 +1,23 @@
-import graph.Bipartition;
-import graph.Edge;
-import graph.Graph;
-import graph.Vertex;
+import graph.*;
 import utils.Algorithm;
 import utils.Cache;
 import utils.GraphToMap;
 import utils.JSONtoGraph;
-import graph.MatchingAlgorithm;
 
-import java.util.List;
 import java.util.Map;
 
 public class Main {
 
   public static void main(String[] args) throws Exception {
-    Graph graph = JSONtoGraph.createVertexList("vertices.json");
+    Graph graph = JSONtoGraph.createVertexList("vertices2.json");
     Map<String, Vertex> lookupTable = GraphToMap.createGraphLookup(graph);
-    //List<Edge> edges = JSONtoGraph.createEdgeStream("edges.json");
-    Cache.setEdgeStreamFileName("edges.json");
+    Cache.setEdgeStreamFileName("edges2.json");
     Algorithm algorithm = new Algorithm(lookupTable);
     Bipartition bipartition = algorithm.bipartition();
     System.out.println(bipartition);
-    MatchingAlgorithm matching = new MatchingAlgorithm(lookupTable);
+    Matching matching = algorithm.createMaximalMatching();
+    MatchingAlgorithm matchingAlgorithm = new MatchingAlgorithm(lookupTable, matching);
     System.out.println(algorithm.createMaximalMatching());
-    matching.findUnweightedMatching(0.2);
+    matchingAlgorithm.findUnweightedMatching(0.2);
   }
 }
--- src/main/java/graph/MatchingAlgorithm.java
+++ src/main/java/graph/MatchingAlgorithm.java
@@ -1,12 +1,14 @@
 package graph;
 
 import java.io.FileNotFoundException;
+import java.util.Iterator;
 import java.util.Map;
-import java.util.stream.Stream;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import utils.Algorithm;
+import utils.Cache;
+import utils.JSONtoGraph;
 import utils.NotBipartiteException;
 
 import static java.lang.Math.*;
@@ -18,18 +20,24 @@ public class MatchingAlgorithm {
   //Spatial complexity of M, number of Vertices
   private final Map<String, Vertex> lookupTable;
 
+  private Matching matching;
+
   public void findUnweightedMatching(double epsilon) throws NotBipartiteException, FileNotFoundException {
     Algorithm algorithm = new Algorithm(lookupTable);
     Bipartition bipartition = algorithm.bipartition();
     for (int i = 1; i < ceil(log(6 * epsilon) / log(8 / 9)); i++) {
       double sigma = epsilon / (2 - 3 * epsilon);
-      findAugmentingPaths(sigma);
+      findAugmentingPaths(bipartition, sigma);
     }
 
   }
 
-  public void findAugmentingPaths(double sigma) {
+  public void findAugmentingPaths(Bipartition bipartition, double sigma) throws FileNotFoundException {
+    Iterator<Edge> iterator = JSONtoGraph.createEdgeStream(Cache.getEdgeStreamFileName()).iterator();
+    while(iterator.hasNext()) {
+      Edge edge = iterator.next();
 
+    }
   }
 
   private void findDisjointLeftWings() {
